0a14fab508250c634d2cfa1a532ff650cd7d6a3b
nathanielksmith <nathanielksmith@gmail.com>
2009-10-03 17:40:53 -0400

    first working version
    Ignore-this: ef683323a2d491bffc1b1716a476852
    
    darcs-hash:20091003214053-6c4b1-76f17bace090a8f454a92363654126d306b2fe8e.gz

--- rss.py
+++ rss.py
@@ -1,62 +1,59 @@
 # what rss.py
 # who  nate smith
 # when oct 2009
-# why  python script for use in independent study
+# why  python class for use in independent study
 
 import pyext
 import urllib2
 import xml.dom.minidom
 
 class rss(pyext._class):
-    current_item_index = 0
-    items              = []
-    url                = ""
+    index = 0
+    items = []
+    url   = ""
+
+    _inlets  = 1 # tried leaving this out, wouldn't let me specify callbacks for outlet 0...
+    _outlets = 1 # tried leaving this out, wouldn't let me access 0th or 1st outlet...
 
     def __init__(self,*args):
+        print "No init args"
         if len(args) > 0:
             self.url = args[0]
-            self._regen_dom()
-            self._populate_items()
+            self._repopulate()
     
-    def bang_0(self):
-        self._outlet(1, "Hello world")
+    def bang_1(self):
+        self._outlet(1, str(self._fetch_next()))
 
-    def url_0(self, arg):
+    def url_1(self, arg):
         arg = str(arg)
         print "Setting URL to " + arg
         self.url = arg
-        self._regen_dom()
-        self._populate_items()
+        self._repopulate()
 
-    def _regen_dom(self):
+    def _repopulate(self):
+        print "Regenerating DOM"
         self.dom = xml.dom.minidom.parseString(urllib2.urlopen(self.url).read())
 
-    def _populate_items(self):
-        self.current_item_index = 0
-        self.items = self.dom.getElementsByTagName("description")
+        print "Repopulating items array"
+        self.index = 0
+        self.items = map(lambda x: x.firstChild.data, self.dom.getElementsByTagName("description"))
+
+        # XXX I want unicode support, but for some reason python makes it hard. Filter unicode strings for now.
+        print "Items now has %s entries. Filtering unicode..." % len(self.items)
+        def can_decode(t):
+            try:
+                str(t)
+                return True
+            except:
+                return False
+        self.items = filter(can_decode, self.items)
+
+        print "Items now has %s entries" % len(self.items)
 
     def _fetch_next(self):
-        if self.current_item_index >= len(self.items):
-            self.current_item_index = 0
-        self._outlet(1, items[self.current_item_index])
-        self.current_item_index += 1
-        
-
-#import sys
-#
-#print "Script init'd"
-#
-#URL = ""
-#
-#try:
-#    URL = sys.argv[1]
-#except:
-#    print "no default args"
-#
-#def fetch_next():
-#    return URL
-#
-#def set_url(url):
-#    URL = url
-
-
+        if self.index >= len(self.items):
+            self._repopulate()
+        print "Trying to fetch item at index %s" % self.index
+        ret = self.items[self.index]
+        self.index += 1
+        return ret
